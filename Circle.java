67a8b1c4b2db25d9d6864faced79de76a86370ab
jekutinamo23 <jekutinamo23@students.desu.edu>
2024-10-11 23:00:40 -0400

    run

--- Circle.java
+++ Circle.java
@@ -3,20 +3,20 @@ import java.util.Random;
 
 /**
  * Write a description of class Circle here.
- * 
- * @author (your name) 
+ *
+ * @author (your name)
  * @version (a version number or a date)
  */
 public class Circle extends Actor
 {
     private final static int MAX_RADIUS = 20;
-    
+
     private int radius;
     private int increment;
     private int maxRadius;
     GreenfootImage image;
     Random rand;
-    
+
     private void init(int maxRadius){
         this.maxRadius = maxRadius;
         increment = maxRadius;
@@ -26,28 +26,32 @@ public class Circle extends Actor
         rand = new Random(17);
         setRotation(90);
     }
-    
+
     public Circle(){
         init(MAX_RADIUS);
     }
-    
+
     public Circle(int maxRadius){
         init(maxRadius);
     }
-    
+
     private void pulsate(){
         image.clear();
         image.fillOval(maxRadius-radius,maxRadius-radius,
-                        2*radius,2*radius);
+                2*radius,2*radius);
         setImage(image);
         increment = (increment+1) % (2*maxRadius);
-        radius = Math.abs(increment-maxRadius);    
+        radius = Math.abs(increment-maxRadius);
     }
-    
+
     private void moveIt(){
-        // TODO replace this comment with your code
+        int randomValue = rand.nextInt(21) - 10;
+        int currentRotation = getRotation();
+        int newRotation = currentRotation + randomValue;
+        setRotation(newRotation);
+        move(1);
     }
-    
+
     /**
      * Act - do whatever the Circle wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
